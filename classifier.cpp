d7ef6d2a7d3bcb913b53bf346be88966ee634891
amacp001 <amacp001@ucr.edu>
2017-03-09 00:14:53 -0800

    Worked on file reading functionality

--- classifier.cpp
+++ classifier.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 
 Data::Data(const char *data_file)
 {
@@ -15,7 +16,18 @@ Data::Data(const char *data_file)
 		std::getline(in_file, cur_line);
 		if(cur_line != "")
 		{
-			std::cout << "Line " << row_cnt << ": " << cur_line << "\n";
+			//std::cout << "Line " << row_cnt << ": " << cur_line << "\n";
+			int col_cnt = 0;
+			char *tok, *s_ptr;
+			char *tok_str = (char *)cur_line.c_str();
+			for(tok = strtok_r(tok_str, " \n", &s_ptr); tok != NULL; tok = strtok_r(NULL, " \n", &s_ptr))
+			{
+				//std::cout << tok << " ";
+				My_atof(tok);
+				col_cnt++;
+			}
+			std::cout << std::endl;
+			feat_cnt = col_cnt-1;
 			row_cnt++;
 		}
 	}
@@ -24,6 +36,12 @@ Data::Data(const char *data_file)
 
 double Data::My_atof(char *ascii_str)
 {
+	char *s_ptr, *tok;
+	double value;
+	tok = strtok_r(ascii_str, "e+", &s_ptr);
+	//std::cout << "Base: " << tok;
+	tok = strtok_r(NULL, "e+", &s_ptr);
+	//std::cout << ", Exponent: " << tok << std::endl;
 	return 0.0;
 }
 
