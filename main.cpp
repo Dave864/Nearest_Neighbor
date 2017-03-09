157ff66f0ecc1e1636292095d3d574c3506f5f30
amacp001 <amacp001@ucr.edu>
2017-03-08 23:14:22 -0800

    Updated the user prompt

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
 #include "classifier.h"
 
 int main(int argc, char **argv)
 {
-	if(argc < 2)
-	{
-		std::cout << "Please enter a file\n";
-		return 0;
-	}
-	Data test(argv[1]);
+	printf("Welcome to Anthony Macpherson's Feature Selection Algorithm.\n");
+	printf("Type in the name of the file to test: ");
+	std::string file;
+	std::getline(std::cin, file);
+	Data test(file.c_str());
+	//printf("Type the number of the algorithm you want to run.\n\n");
+	//printf("\t1) Forward Selection\n\t2) Backward Selection\n\t3) Anthony's Special Algorithm\n\n");
 	return 0;
 }
